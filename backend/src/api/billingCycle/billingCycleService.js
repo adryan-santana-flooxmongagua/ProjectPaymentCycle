a8776c2e4fb535ed0a833285bc5241692d1093bd
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-03-31 11:09:08 -0300

    fix: correct GET method

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -4,4 +4,22 @@ const errorHandler = require('../common/errorHandler')
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
 
+BillingCycle.route('get', (req, res, next) => {
+
+    BillingCycle.find({}, (err, docs) => {
+
+        if(!err) {
+
+            res.json(docs)
+
+        } else {
+
+            res.status(500).json({errors: [error]})
+
+        }
+
+    })
+
+})
+
 module.exports = BillingCycle
