8cc249579239837ff2843e8da5577f2fb5c3a0ab
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-03-31 11:59:17 -0300

    feat: get record count

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -4,22 +4,16 @@ const errorHandler = require('../common/errorHandler')
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
 
-BillingCycle.route('get', (req, res, next) => {
-
-    BillingCycle.find({}, (err, docs) => {
-
-        if(!err) {
-
-            res.json(docs)
-
-        } else {
-
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count((error, value) => {
+        if(error) {
             res.status(500).json({errors: [error]})
-
+        } else {
+            res.json({value})
         }
-
     })
-
 })
+
+
 
 module.exports = BillingCycle
