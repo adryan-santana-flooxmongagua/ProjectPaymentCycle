5875bc6a924e9172d2a30a3be501de04a810c125
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-04-01 09:54:34 -0300

    feat: create Footer component

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,11 +3,16 @@ import React from 'react'
 
 import Header from '../common/template/header'
 import SideBar from '../common/template/sideBar'
+import Footer from '../common/template/footer'
 
 export default props => (
     <div className='wrapper'>
-       <Header/>
-       <SideBar />
-      
+        <Header />
+        <SideBar />
+        <div className='content-wrapper'> 
+            <h1>aap</h1>
+        </div>
+        <Footer />
+        
     </div>
 )
